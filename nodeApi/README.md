db2768af357dfb049c12da3cfdb370b0958a1e67
Nikunj Patodiya <patodiyanikunj@hotmail.com>
2019-07-23 21:29:34 +0530

    initial commit backend
    socket-io-chatboat-back-end

---
+++ nodeApi/README.md
@@ -0,0 +1,34 @@
+# Chat Room with Socket.io
+
+A sample chatroom using [socket.io](http://socket.io) and Nodes.js/Express based of the tutorial found [here](http://socket.io/get-started/chat/).
+
+## Usage
+
+Install dependencies:
+
+```shell
+$ npm install
+```
+
+Run server locally:
+
+```shell
+$ npm start
+```
+
+Access the client(s) via visiting: `http://localhost:3000`
+
+App structure:
+
+```
+.
+├── api                   # API files
+|   ├── router            # API router files
+|   |   ├── home.js       # API routes
+|   |   ├── index.js      # Router exports (mounts routes)
+│   ├── socket            # API socket files
+|   |   ├── index.js      # Server Socket.io implementation
+│   └── index.js          # API exports
+└── server.js             # Express entry point file
+```
+__Note__: Runs on Node.js v4.2.2+
---
+++ nodeApi/api/socket/index.js
@@ -0,0 +1,103 @@
+'use strict';
+/* api/socket/indjex.js */
+
+const Moniker = require('moniker');
+const nameGen = Moniker.generator([Moniker.adjective, Moniker.noun]);
+
+const _users = new Map();
+
+/*
+  Pickes a random user nickname based off of a socket ID. If such nickname
+  already exists, then a numeric suffix will be added.
+*/
+let getUser = (id) => {
+  let nickname = _users.get(id);
+
+  // If this ID has not been encountered before
+  if (!nickname) {
+    nickname  = nameGen.choose();
+    let count = 0;
+
+    // Prevent collisions by appending a numeric suffix
+    for (let name of _users.values()) {
+      if (name === nickname) {
+        count += 1;
+      }
+    }
+
+    if (count !== 0) {
+      nickname += ` (${count})`;
+    }
+
+    // Store the ID nickname pair in the map
+    _users.set(id, nickname);
+  }
+
+  return nickname;
+};
+
+/*
+  Configure socket.io events.
+*/
+let clients = [];
+let bootstrap = (io) => {
+  // connection event
+  io.on('connection', (socket) => {
+    socket.username = getUser(socket.id);
+    clients.push(socket.id);
+
+    socket.emit('notify user', socket.username);
+    socket.broadcast.emit('user connected', socket.username);
+
+    // disconnect event
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('user disconnected', socket.username);
+    });
+
+    // chat message event
+    socket.on('chat message', (params) => {
+      let timestamp = (new Date()).toISOString();
+
+      io.emit('chat message', {
+        nickname: socket.username,
+        message: params.message,
+        time: timestamp
+      });
+    });
+    // add new user
+    socket.on('add user', (user) => {
+      clients[user] = {
+        "socket": socket.id
+      };
+      io.emit('user connected', user);
+    });
+    
+    // user name change event
+    socket.on('user namechange', (newName) => {
+      socket.nickname = newName;
+      socket.emit('user namechange', {
+        nickname: socket.nickname
+      });
+      io.emit('notify user', socket.nickname)
+    });
+
+    // user typing event
+    socket.on('user typing', (isTyping) => {
+      if (isTyping === true) {
+        socket.broadcast.emit('user typing', {
+          nickname: socket.username,
+          isTyping: true
+        });
+      } else {
+        socket.broadcast.emit('user typing', {
+          nickname: socket.username,
+          isTyping: false
+        });
+      }
+    });
+  });
+};
+
+module.exports = {
+  bootstrap: bootstrap
+};
